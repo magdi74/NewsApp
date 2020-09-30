70d30f77ebfc42ccd2cdc342d000738f4c78e9de
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-30 20:35:49 +0200

    Added Data Repository object

---
+++ app/src/main/java/com/example/newsapp/datarepository/NewsRepository.kt
@@ -0,0 +1,48 @@
+package com.example.newsapp.datarepository
+
+import com.example.newsapp.apicalls.ApiServiceInterface
+import com.example.newsapp.constants.Constants
+import com.example.newsapp.models.Article
+import com.example.newsapp.models.ArticlesResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NewsRepository {
+    private var apiServices: ApiServiceInterface
+    init{
+        val  retrofit = Retrofit
+            .Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        apiServices = retrofit.create(ApiServiceInterface::class.java)
+    }
+
+    fun fetchNewsHeadlines(
+        page: Int,
+        onSuccess: (headlines: MutableList<Article>) -> Unit,
+        onError: () -> Unit
+    ){
+        apiServices.getHeadlines(page = page)
+            .enqueue(object: Callback<ArticlesResponse>{
+                override fun onResponse(
+                    call: Call<ArticlesResponse>,
+                    response: Response<ArticlesResponse>
+                ) {
+                    if(response.isSuccessful){
+                        if(response.body() != null)
+                            onSuccess.invoke(response.body()!!.articles)
+                    }
+                    else onError.invoke()
+                }
+
+                override fun onFailure(call: Call<ArticlesResponse>, t: Throwable) = onError.invoke()
+            })
+    }
+
+
+
+}
