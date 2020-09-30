87fe3961eef3e0af283d72e4eb43060def18918a
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-30 20:23:16 +0200

    Added Return types to Interface Methods

--- app/src/main/java/com/example/newsapp/apicalls/ApiServiceInterface.kt
+++ app/src/main/java/com/example/newsapp/apicalls/ApiServiceInterface.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp.apicalls
 
 import com.example.newsapp.constants.Constants
+import com.example.newsapp.models.ArticlesResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,12 +12,12 @@ interface ApiServiceInterface {
         @Query("apikey") apiKey: String = Constants.API_KEY,
         @Query("language") country: String = "en",
         @Query("page") page: Int = 1
-    ) /*: Call<>*/
+    ) : Call<ArticlesResponse>
 
     @GET("/v2/everything")
     fun getEverything(
         @Query("apikey") apiKey: String = Constants.API_KEY,
         @Query("language") country: String = "en",
         @Query("page") page: Int = 1
-    ) /*: Call<> */
+    ) : Call<ArticlesResponse>
 }
