21c0adf180e59079c64fcc44d751e6858b6a6c38
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-30 19:09:13 +0200

    Added Constants Static Members with API Key Value

---
+++ app/src/main/java/com/example/newsapp/constants/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.constants
+
+class Constants{
+    companion object {
+        val API_KEY = "1297c4f70b5e4824afd978523aa9b0a6"
+        val BASE_URL = ""
+        val IMAGE_URL = ""
+    }
+}
