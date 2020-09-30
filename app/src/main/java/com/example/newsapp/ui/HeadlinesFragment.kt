d5efc4d8b87012793dffce257be8786f19123f2b
Khaled <khaledmedhat64@gmail.com>
2020-10-01 01:07:07 +0200

    Added fragment layout resource files and their respective classes

---
+++ app/src/main/java/com/example/newsapp/ui/HeadlinesFragment.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.newsapp.R
+
+class HeadlinesFragment : Fragment(), HeadlinesAdapter.HeadlineListener {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_headlines, container, false)
+
+    override fun headlineClicked(id: Int) {
+
+        Toast.makeText(context, id , Toast.LENGTH_SHORT).show()
+
+    }
+
+
+}
