6a07aa38d0f2193dcf29a5ef3ee2eaa3bf61a85b
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-30 20:20:25 +0200

    Added Articles Response Data Model Class

---
+++ app/src/main/java/com/example/newsapp/models/ArticlesResponse.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.models
+
+import com.google.gson.annotations.SerializedName
+
+data class ArticlesResponse(
+    @SerializedName("articles") var articles: MutableList<Article>,
+    @SerializedName("page") var currentPage: Int
+)
