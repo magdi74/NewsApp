6e40dd67a11a58d3bb5cbe00a9bffce0b9adcd45
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-30 20:12:53 +0200

    Added page parameter to allow pagination

--- app/src/main/java/com/example/newsapp/apicalls/ApiServiceInterface.kt
+++ app/src/main/java/com/example/newsapp/apicalls/ApiServiceInterface.kt
@@ -9,12 +9,14 @@ interface ApiServiceInterface {
     @GET("/v2/top-headlines")
     fun getHeadlines(
         @Query("apikey") apiKey: String = Constants.API_KEY,
-        @Query("language") country: String = "en"
+        @Query("language") country: String = "en",
+        @Query("page") page: Int = 1
     ) /*: Call<>*/
 
     @GET("/v2/everything")
     fun getEverything(
         @Query("apikey") apiKey: String = Constants.API_KEY,
-        @Query("language") country: String = "en"
+        @Query("language") country: String = "en",
+        @Query("page") page: Int = 1
     ) /*: Call<> */
 }
