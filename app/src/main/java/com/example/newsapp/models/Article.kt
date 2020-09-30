48a0e309e6fe7aa6e3d71b9278689d0d536aaa5f
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-30 20:24:11 +0200

    Added SerializedName annotation to Model Classes

--- app/src/main/java/com/example/newsapp/models/Article.kt
+++ app/src/main/java/com/example/newsapp/models/Article.kt
@@ -1,12 +1,14 @@
 package com.example.newsapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Article(
-    var source: Source,
-    var author: String,
-    var title: String,
-    var description: String,
-    var url: String,
-    var imageUrl: String,
-    var publishedAt: String,
-    var content: String
+    @SerializedName("source") var source: Source,
+    @SerializedName("author") var author: String,
+    @SerializedName("title") var title: String,
+    @SerializedName("description") var description: String,
+    @SerializedName("url") var url: String,
+    @SerializedName("urlToImage") var imageUrl: String,
+    @SerializedName("publishedAt") var publishedAt: String,
+    @SerializedName("content") var content: String
 )
--- app/src/main/java/com/example/newsapp/models/Source.kt
+++ app/src/main/java/com/example/newsapp/models/Source.kt
@@ -1,6 +1,8 @@
 package com.example.newsapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Source(
-    val id: String,
-    val name: String
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String
 )
