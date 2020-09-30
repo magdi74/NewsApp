b57831b23415e49e797f52d2db4e6d70bf3d39fe
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-30 21:14:59 +0200

    Added Status Field in ArticlesResponse data class

--- app/src/main/java/com/example/newsapp/models/ArticlesResponse.kt
+++ app/src/main/java/com/example/newsapp/models/ArticlesResponse.kt
@@ -4,5 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class ArticlesResponse(
     @SerializedName("articles") var articles: MutableList<Article>,
-    @SerializedName("page") var currentPage: Int
+    @SerializedName("page") var currentPage: Int,
+    @SerializedName("status") var status: String
 )
