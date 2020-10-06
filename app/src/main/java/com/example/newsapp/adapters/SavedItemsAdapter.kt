17ab5c3e3d4e4e865e509f76b52ff44c3533efd5
AlaaMagdy <alaa.m.elmaghraby@gmail.com>
2020-10-06 16:09:20 +0200

    SavedItems

---
+++ app/src/main/java/com/example/newsapp/adapters/SavedItemsAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.newsapp.adapters
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.example.newsapp.Item
+import com.example.newsapp.R
+import com.example.newsapp.models.Article
+import kotlinx.android.synthetic.main.news_card.view.*
+
+
+class SavedItemsAdapter(private val List: MutableList<Article>?, var Listener: SavedItemsListener):
+    RecyclerView.Adapter<SavedItemsAdapter.NewsViewHolder>() {
+    public interface SavedItemsListener {
+        fun savedItemsClicked(article: Article)
+    }
+    inner class NewsViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        fun onBind(article: Article){
+            itemView.article_headline.text= article.title
+            itemView.article_source.text = article.source.toString()
+            itemView.article_date.text = article.publishedAt
+            Glide.with(itemView).load(article.imageUrl).transform(CenterCrop()).into(itemView.article_poster)
+            var saveState = 0
+
+            itemView.setOnClickListener({Listener.savedItemsClicked(article)})
+
+            itemView.btn_save.setOnClickListener({
+                if (saveState == 0) {
+                    itemView.btn_save.setImageResource(R.drawable.ic_saved)
+                    saveState = 1
+                }
+                else{
+                    itemView.btn_save.setImageResource(R.drawable.ic_unsaved)
+                    saveState = 0
+                }
+            })
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder
+            = NewsViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.saved_articles_card,parent,false))
+    override fun getItemCount(): Int= List!!.size
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.onBind(List!![position])
+    }
+
+    fun appendNews(articles : List<Article>)
+    {
+        this.List!!.addAll(articles)
+        notifyItemRangeInserted(this.List!!.size,this.List.size-1)
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val imageView: ImageView = itemView.article_poster
+        val textView1: TextView = itemView.article_headline
+        val textView2: TextView = itemView.article_source
+        val textView3: TextView = itemView.article_date
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/newsapp/ui/SavedItemsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/SavedItemsFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.adapters.HeadlinesAdapter
 
 class SavedItemsFragment : Fragment() {
+
+   /** lateinit var newsAdapter: HeadlinesAdapter
+    lateinit var llm: LinearLayoutManager **/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_saved_items, container, false)
+    ): View? {
 
+        val view: View = inflater.inflate(R.layout.fragment_saved_items, container, false)
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 }
