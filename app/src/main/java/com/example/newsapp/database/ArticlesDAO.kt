60da16a4e5c29a001402713dc9402e7de9aef7f6
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-10-07 18:48:40 +0200

    Added Articles Data Access Object Class

--- app/src/main/java/com/example/newsapp/database/ArticlesDAO.kt
+++ app/src/main/java/com/example/newsapp/database/ArticlesDAO.kt
@@ -1,4 +1,21 @@
 package com.example.newsapp.database
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.newsapp.models.Article
+
 interface ArticlesDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMovieList(movieList: MutableList<Article>)
+
+    @Query("Select * from articles_table where saved = 'true'")
+    fun getSavedArticles(): MutableList<ArticleEntity>
+
+    @Query("Select * from articles_table")
+    fun getCachedArticles(): MutableList<ArticleEntity>
+
+    @Update
+    fun updateArticleSaveStatus(article: Article)
 }
