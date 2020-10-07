da281a2ec5b0f5e824bf7ff15f8d070700b0864b
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-10-07 18:43:53 +0200

    Added Article Entity Model Class

--- app/src/main/java/com/example/newsapp/database/ArticleEntity.kt
+++ app/src/main/java/com/example/newsapp/database/ArticleEntity.kt
@@ -1,8 +1,18 @@
 package com.example.newsapp.database
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.newsapp.models.Source
 
 @Entity(tableName = "articles_table")
-class ArticleEntity {
-    
-}
+class ArticleEntity (
+    var source: Source,
+    var author: String,
+    var title: String,
+    var description: String,
+    @PrimaryKey var url: String,
+    var imageUrl: String,
+    var publishedAt: String,
+    var content: String,
+    var saved: Boolean
+    )
