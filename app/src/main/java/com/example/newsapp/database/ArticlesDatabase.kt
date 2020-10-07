2883864a37ff9413e8dd1ee1d30ab05e8c8745f5
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-10-07 18:57:49 +0200

    Implemented the Articles Database Class

--- app/src/main/java/com/example/newsapp/database/ArticlesDatabase.kt
+++ app/src/main/java/com/example/newsapp/database/ArticlesDatabase.kt
@@ -1,6 +1,23 @@
 package com.example.newsapp.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [ArticleEntity::class], version = 1, exportSchema = false)
 abstract class ArticlesDatabase: RoomDatabase() {
+
+    companion object{
+        var articlesDatabase: ArticlesDatabase? = null
+
+        fun getDatabaseInstance(context: Context): ArticlesDatabase{
+            if(articlesDatabase == null)
+                articlesDatabase = Room.databaseBuilder(context, ArticlesDatabase::class.java,"ArticlesDatabase")
+                                    .fallbackToDestructiveMigration()
+                                    .build()
+            return articlesDatabase!!
+        }
+    }
+    abstract fun ArticlesDao(): ArticlesDAO
 }
