2eba27c1d7ffc91810538718fb6c816f0cdeaa21
Mohamed Magdy <magdim200@gmail.com>
2020-10-08 21:48:41 +0200

    Allowed for Nullability

--- app/src/main/java/com/example/newsapp/database/ArticleEntity.kt
+++ app/src/main/java/com/example/newsapp/database/ArticleEntity.kt
@@ -1,18 +1,19 @@
 package com.example.newsapp.database
 
+import androidx.annotation.Nullable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.newsapp.models.Source
 
 @Entity(tableName = "articles_table")
 class ArticleEntity (
-    var source: Source,
-    var author: String,
-    var title: String,
-    var description: String,
+    var source: Source?,
+    var author: String?,
+    var title: String?,
+    var description: String?,
     @PrimaryKey var url: String,
-    var imageUrl: String,
-    var publishedAt: String,
-    var content: String,
-    var saved: Boolean
+    var imageUrl: String?,
+    var publishedAt: String?,
+    var content: String?,
+    var saved: Boolean?
     )
