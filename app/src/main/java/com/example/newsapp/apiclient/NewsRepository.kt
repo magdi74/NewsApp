bade021476464c505a53efe2b86ec65481528cbf
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-10-09 23:19:25 +0200

    Updated Repository Functions

--- app/src/main/java/com/example/newsapp/apiclient/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/apiclient/NewsRepository.kt
@@ -8,6 +8,7 @@ import com.example.newsapp.database.ArticlesDAO
 import com.example.newsapp.database.ArticlesDatabase
 import com.example.newsapp.models.Article
 import com.example.newsapp.models.ArticlesResponse
+import com.example.newsapp.models.Source
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,64 +16,90 @@ import retrofit2.Response
 object NewsRepository {
     private var apiClient = NewsClient.apiServices
 
-    private lateinit var database: ArticlesDAO
+    private lateinit var appDatabase: ArticlesDatabase
 
     var articlesList: MutableList<ArticleEntity> = mutableListOf()
 
-    fun getAllArticles(context: Context,
-    onSuccess: (entities: MutableList<ArticleEntity>) ->  Unit,
-    onFailure: (entities: MutableList<ArticleEntity>) -> Unit) {
+    fun initDataBase(context: Context) {
+        appDatabase = ArticlesDatabase.getDatabaseInstance(context)
+    }
 
-        var articlesMutableLiveData: MutableLiveData<MutableList<ArticleEntity>> = MutableLiveData()
+    fun getNews(): LiveData<MutableList<ArticleEntity>> {
+        val newsListLiveData: MutableLiveData<MutableList<ArticleEntity>> = MutableLiveData()
 
-        if(articlesList.isNotEmpty()){
-            //articlesMutableLiveData.postValue(articlesList)
-            onSuccess.invoke(articlesList)
+        if (articlesList.isNotEmpty()) {
+            newsListLiveData.postValue(articlesList)
+            return newsListLiveData
         }
 
-        database = ArticlesDatabase.getDatabaseInstance(context).ArticlesDao()
+        apiClient.getTopHeadlines().enqueue(object : Callback<ArticlesResponse> {
 
-        apiClient.getTopHeadlines().enqueue(object: Callback<ArticlesResponse>{
-            override fun onResponse(
-                call: Call<ArticlesResponse>,
-                response: Response<ArticlesResponse>
-            ) {
-                if(response.isSuccessful){
+            override fun onResponse(call: Call<ArticlesResponse>, response: Response<ArticlesResponse>) {
+
+                if (response.isSuccessful) {
+
                     var remoteList: MutableList<Article> = response.body()?.articles ?: mutableListOf()
 
                     var apiEntity = toEntity(remoteList)
 
-                    var savedFromDatabase = database.getSavedArticles()
+                    articlesList.addAll(apiEntity)
 
-                    var dataset = setSavedStatus(apiEntity,savedFromDatabase)
+                    appDatabase.ArticlesDao().insertArticles(articlesList)
 
-                    database.insertArticles(dataset)
-
-                    onSuccess.invoke(dataset)
-
-                 //   articlesMutableLiveData.postValue(dataset)
-                }
-                else{
-                    //articlesMutableLiveData.postValue(database.getCachedArticles())
-                    onSuccess.invoke(database.getCachedArticles())
+                    newsListLiveData.postValue(articlesList)
+                } else {
+                    val savedNews = appDatabase.ArticlesDao().getCachedArticles()
+                    newsListLiveData.postValue(savedNews)
                 }
             }
 
             override fun onFailure(call: Call<ArticlesResponse>, t: Throwable) {
-                //articlesMutableLiveData.postValue(database.getCachedArticles())
-                onFailure(database.getCachedArticles())
+
+                val savedMovies = appDatabase.ArticlesDao().getCachedArticles()
+                newsListLiveData.postValue(savedMovies)
             }
+
         })
+
+        return newsListLiveData
     }
 
-    fun getSavedArticles(context: Context, onLoaded: (savedArticles: MutableList<ArticleEntity>) -> Unit){
-        database = ArticlesDatabase.getDatabaseInstance(context).ArticlesDao()
-        onLoaded.invoke(database.getSavedArticles())
+    private fun toEntity(modelList: MutableList<Article>): MutableList<ArticleEntity>{
+        var entityList: MutableList<ArticleEntity> = mutableListOf()
+        for( i in 0 until modelList.size){
+            var model = modelList[i]
+            entityList.add(
+                ArticleEntity(
+                    source = Source(model.source.id, model.source.name),
+                    author = model.author,
+                    title = model.title,
+                    description = model.description,
+                    url = model.url,
+                    imageUrl = model.imageUrl,
+                    publishedAt = model.publishedAt,
+                    content = model.content,
+                    saved = false
+                )
+            )
+        }
+        return entityList
     }
 
-    fun updateSavedArticles(context: Context, article: ArticleEntity){
-        database = ArticlesDatabase.getDatabaseInstance(context).ArticlesDao()
-        database.updateArticleSaveStatus(article)
+    fun getSavedArticles(): LiveData<MutableList<ArticleEntity>>{
+
+        val newsListLiveData: MutableLiveData<MutableList<ArticleEntity>> = MutableLiveData()
+
+        var savedList: MutableList<ArticleEntity> = mutableListOf()
+        savedList = appDatabase.ArticlesDao().getSavedArticles()
+
+        if (savedList.isNotEmpty()) {
+            newsListLiveData.postValue(savedList)
+        }
+        return newsListLiveData
+
+    }
+
+    fun updateArticles(){
     }
 
     private fun setSavedStatus(apiEntities: MutableList<ArticleEntity>, saved: MutableList<ArticleEntity>): MutableList<ArticleEntity>{
@@ -89,26 +116,5 @@ object NewsRepository {
             }
         }
         return articleEntities
-    }
-
-    private fun toEntity(modelList: MutableList<Article>): MutableList<ArticleEntity>{
-        var entityList: MutableList<ArticleEntity> = mutableListOf()
-        for( i in 0 until modelList.size){
-            var model = modelList[i]
-            entityList.add(
-                ArticleEntity(
-                    source = model.source,
-                    author = model.author,
-                    title = model.title,
-                    description = model.description,
-                    url = model.url,
-                    imageUrl = model.imageUrl,
-                    publishedAt = model.publishedAt,
-                    content = model.content,
-                    saved = false
-                )
-            )
-        }
-        return entityList
     }
 }
