1e1deddccf6bdb1579edc9060756382736195af1
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-10-10 01:07:33 +0200

    Added Observers and Adapters for Saved and Headlines recyclerviews and implemented the interfaces of the adapters

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -16,6 +16,7 @@ import com.example.newsapp.models.Article
 import com.example.newsapp.viewmodel.ArticlesViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_headlines.*
+import kotlinx.android.synthetic.main.fragment_saved_items.*
 
 class MainActivity :  AppCompatActivity() , HeadlinesAdapter.HeadlineListener, SavedItemsAdapter.SavedItemsListener {
 
@@ -28,13 +29,20 @@ class MainActivity :  AppCompatActivity() , HeadlinesAdapter.HeadlineListener, S
         var headlinesAdapter = HeadlinesAdapter(mutableListOf(), this)
         var headlinesLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
 
+        var savedAdapter = SavedItemsAdapter(mutableListOf(), this)
+        var savedLayoutManager  = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
         rv_headlines.adapter = headlinesAdapter
         rv_headlines.layoutManager = headlinesLayoutManager
+
+
+        articlesViewModel.callNews()
 
         articlesViewModel.getNews()
             .observe(this, Observer{
                 rv_headlines.adapter = HeadlinesAdapter(it as MutableList<ArticleEntity>?, this)
             })
+
 
         bottomNavMenu!!.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
@@ -46,6 +54,16 @@ class MainActivity :  AppCompatActivity() , HeadlinesAdapter.HeadlineListener, S
                 R.id.savedItemsFragment -> {
                     var fragManager = supportFragmentManager
                     fragManager.beginTransaction().show(savedFrag).hide(headLinesFrag).hide(detailsFrag).commit()
+
+                    rv_saved.adapter = savedAdapter
+                    rv_saved.layoutManager = savedLayoutManager
+
+                    articlesViewModel.callSavedArticles()
+
+                    articlesViewModel.getSavedArticles().observe(this, Observer{
+                        rv_saved.adapter = SavedItemsAdapter(it as MutableList<ArticleEntity>?, this)
+                    })
+
                     true
                 }
                 else -> false
@@ -59,7 +77,17 @@ class MainActivity :  AppCompatActivity() , HeadlinesAdapter.HeadlineListener, S
 
     }
 
+    override fun headlineSaveStatus(article: ArticleEntity) {
+        articlesViewModel.updateSaved(article)
+        articlesViewModel.callNews()
+        articlesViewModel.callSavedArticles()
+    }
+
     override fun savedItemsClicked(article: ArticleEntity) {
+
+    }
+
+    override fun savedItemsSaved(article: ArticleEntity) {
 
     }
 
