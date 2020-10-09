befd26e78f962ee4a6e90c47a7c47118f422c261
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-10-09 23:30:30 +0200

    Removed unwanted line of code for reconstruction

--- app/src/main/java/com/example/newsapp/ui/ItemDetailsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/ItemDetailsFragment.kt
@@ -1,22 +1,11 @@
 package com.example.newsapp.ui.destinations
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.Context.*
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.content.ContextCompat.getSystemServiceName
 import com.example.newsapp.*
-import com.example.newsapp.models.Article
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_headlines.*
-import kotlinx.android.synthetic.main.fragment_item_details.*
 import kotlinx.android.synthetic.main.fragment_item_details.view.*
 
 class ItemDetailsFragment : Fragment() {
@@ -31,18 +20,7 @@ class ItemDetailsFragment : Fragment() {
 
 
         view.btnSave.setOnClickListener{ item ->
-            if (saveState == 0) {
-                btnSave.setImageResource(R.drawable.ic_saved)
-                articleMain.saved=true
-                saveState = 1
-                savedlistTest.add(articleMain)
-            }
-            else{
-                btnSave.setImageResource(R.drawable.ic_unsaved)
-                articleMain.saved = false
-                saveState = 0
-                savedlistTest.remove(articleMain)
-            }
+
         }
         return view
     }
@@ -53,12 +31,4 @@ class ItemDetailsFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
     }
-
-
-
-
-
-
-
-
 }
